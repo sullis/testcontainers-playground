822bae860e9b1aa71ef4bd646f64589ffd2e4e36
sullis <github@seansullivan.com>
2024-04-28 10:32:06 -0700

    s3 async clients

--- src/test/java/io/github/sullis/testcontainers/playground/LocalstackTest.java
+++ src/test/java/io/github/sullis/testcontainers/playground/LocalstackTest.java
@@ -1,6 +1,7 @@
 package io.github.sullis.testcontainers.playground;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
@@ -28,7 +29,6 @@ import software.amazon.awssdk.services.kinesis.KinesisClient;
 import software.amazon.awssdk.services.kinesis.model.CreateStreamResponse;
 import software.amazon.awssdk.services.kinesis.model.PutRecordResponse;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.S3AsyncClientBuilder;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
 import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
@@ -66,7 +66,8 @@ public class LocalstackTest {
   private static final List<SdkHttpClient.Builder<?>> SYNC_HTTP_CLIENT_BUILDER_LIST= List.of(
       ApacheHttpClient.builder());
   private static final List<SdkAsyncHttpClient.Builder<?>> ASYNC_HTTP_CLIENT_BUILDER_LIST = List.of(
-      NettyNioAsyncHttpClient.builder());
+      NettyNioAsyncHttpClient.builder(),
+      AwsCrtAsyncHttpClient.builder());
 
   private static final LocalStackContainer LOCALSTACK = new LocalStackContainer(DockerImageName.parse("localstack/localstack:3.3.0"))
       .withServices(LocalStackContainer.Service.DYNAMODB, LocalStackContainer.Service.S3, LocalStackContainer.Service.KINESIS);
@@ -164,15 +165,24 @@ public class LocalstackTest {
     assertSuccess(deleteTableResponse);
   }
 
-  public static List<S3AsyncClient> s3AsyncClients() {
-    var builder = (S3AsyncClientBuilder) configure(S3AsyncClient.builder());
-    var crtBuilder = (S3CrtAsyncClientBuilder) configure(S3AsyncClient.crtBuilder());
-    return List.of(builder.build(), crtBuilder.build());
+  public static List<S3AsyncClientInfo> s3AsyncClients() {
+    List<S3AsyncClientInfo> result = new ArrayList<>();
+    ASYNC_HTTP_CLIENT_BUILDER_LIST.forEach(httpClientBuilder -> {
+      var httpClient = httpClientBuilder.build();
+      S3AsyncClient s3Client = (S3AsyncClient) configure(S3AsyncClient.builder().httpClient(httpClient)).build();
+      result.add(new S3AsyncClientInfo(httpClient.clientName(), s3Client));
+    });
+
+    // S3 crtBuilder
+    result.add(new S3AsyncClientInfo("crtBuilder", configure(S3AsyncClient.crtBuilder()).build()));
+
+    return result;
   }
 
   @ParameterizedTest
   @MethodSource("s3AsyncClients")
-  public void s3(S3AsyncClient s3Client) throws Exception {
+  public void s3(S3AsyncClientInfo s3ClientInfo) throws Exception {
+    final S3AsyncClient s3Client = s3ClientInfo.client;
     final byte[] payload = "payload123".getBytes(StandardCharsets.UTF_8);
     final String bucket = "bucket-" + UUID.randomUUID();
     final String pathToFile = "/path/" + UUID.randomUUID();
@@ -247,5 +257,12 @@ public class LocalstackTest {
 
   private static void assertSuccess(final SdkResponse sdkResponse) {
     assertThat(sdkResponse.sdkHttpResponse().isSuccessful()).isTrue();
+  }
+
+  public record S3AsyncClientInfo(String description, S3AsyncClient client) {
+    @Override
+    public String toString() {
+      return this.description + ":" + this.client.getClass().getSimpleName();
+    }
   }
 }
